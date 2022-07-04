5b9726e61fa6c1eaa70e1fa9011580001fd7a1ef
Starinin Andrey <starininandrey@gmail.com>
2022-07-04 17:44:54 +0300

    Update

---
+++ CollectionDemo.DictionaryDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CollectionDemo.DictionaryDemo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("one", "один");
+            dict.Add("two", "два");
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+            var keys = dict.Keys;
+            foreach (var key in keys)
+            {
+                Console.Write($"{key} ");
+            }
+            Console.WriteLine();
+            
+            var values = dict.Values;
+            foreach (var value in values)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+
+            dict["five"] = "пять";
+            Console.WriteLine(dict["five"]);
+        }
+    }
+}
---
+++ CollectionDemo.ListDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CollectionDemo.ListDemo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+
+            foreach (var i in list)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            
+            list[0] = 67890;
+            Console.WriteLine(list[0]);
+        }
+    }
+}
